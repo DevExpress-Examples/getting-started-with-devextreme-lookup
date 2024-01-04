453ccb9ff049b0c4656e1f8dba896589037479c9
NLS <nc.rademaker@gmail.com>
2024-01-04 16:43:31 +0100

    update to angular 17 with standalone components, devexpress 23.2.3

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { Service, Task } from './app.service';
 import DataSource from 'devextreme/data/data_source';
 
+import Lookup, { Properties } from 'devextreme/ui/lookup';
+import { DxLookupModule } from 'devextreme-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [Service],
+  standalone: true,
+  imports: [
+    DxLookupModule
+  ]
 })
 export class AppComponent {
   employeesTasks: Task[];
@@ -21,6 +27,10 @@ export class AppComponent {
       paginate: true,
       pageSize: 2
     });
+
+    Lookup.defaultOptions<Properties>({ device: d => true, options: {
+      showClearButton: true,
+    } });
   }
 
   onValueChanged(e: any) {
